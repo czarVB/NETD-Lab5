da5b907d0433b8dc5ea612f9c3e3e05afc5ba7cd
Czar <48457506+czarVB@users.noreply.github.com>
2019-04-05 22:06:05 -0400

    Text Editor (Release)
    The finished copy of the NETD Lab 5 Text Editor

---
+++ NETD-Lab5-EH-Final/NETD-Lab5-EH/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTextEditor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.mnsMenu = New System.Windows.Forms.MenuStrip()
+        Me.tsmFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiClose = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiPaste = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tmiAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.txtContent = New System.Windows.Forms.TextBox()
+        Me.tltToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.mnsMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnsMenu
+        '
+        Me.mnsMenu.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.mnsMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmFile, Me.tsmEdit, Me.tsmHelp})
+        Me.mnsMenu.Location = New System.Drawing.Point(0, 0)
+        Me.mnsMenu.Name = "mnsMenu"
+        Me.mnsMenu.Size = New System.Drawing.Size(582, 28)
+        Me.mnsMenu.TabIndex = 2
+        Me.mnsMenu.Text = "MenuStrip1"
+        '
+        'tsmFile
+        '
+        Me.tsmFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tmiNew, Me.tmiOpen, Me.tmiSave, Me.tmiSaveAs, Me.tmiClose, Me.tmiExit})
+        Me.tsmFile.Name = "tsmFile"
+        Me.tsmFile.Size = New System.Drawing.Size(44, 24)
+        Me.tsmFile.Text = "File"
+        '
+        'tmiNew
+        '
+        Me.tmiNew.Name = "tmiNew"
+        Me.tmiNew.Size = New System.Drawing.Size(181, 26)
+        Me.tmiNew.Text = "New"
+        '
+        'tmiOpen
+        '
+        Me.tmiOpen.Name = "tmiOpen"
+        Me.tmiOpen.Size = New System.Drawing.Size(181, 26)
+        Me.tmiOpen.Text = "Open"
+        '
+        'tmiSave
+        '
+        Me.tmiSave.Name = "tmiSave"
+        Me.tmiSave.Size = New System.Drawing.Size(181, 26)
+        Me.tmiSave.Text = "Save"
+        '
+        'tmiSaveAs
+        '
+        Me.tmiSaveAs.Name = "tmiSaveAs"
+        Me.tmiSaveAs.Size = New System.Drawing.Size(181, 26)
+        Me.tmiSaveAs.Text = "Save As"
+        '
+        'tmiClose
+        '
+        Me.tmiClose.Name = "tmiClose"
+        Me.tmiClose.Size = New System.Drawing.Size(181, 26)
+        Me.tmiClose.Text = "Close"
+        '
+        'tmiExit
+        '
+        Me.tmiExit.Name = "tmiExit"
+        Me.tmiExit.Size = New System.Drawing.Size(181, 26)
+        Me.tmiExit.Text = "Exit"
+        '
+        'tsmEdit
+        '
+        Me.tsmEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tmiCut, Me.tmiCopy, Me.tmiPaste})
+        Me.tsmEdit.Name = "tsmEdit"
+        Me.tsmEdit.Size = New System.Drawing.Size(47, 24)
+        Me.tsmEdit.Text = "Edit"
+        '
+        'tmiCut
+        '
+        Me.tmiCut.Name = "tmiCut"
+        Me.tmiCut.Size = New System.Drawing.Size(181, 26)
+        Me.tmiCut.Text = "Cut"
+        '
+        'tmiCopy
+        '
+        Me.tmiCopy.Name = "tmiCopy"
+        Me.tmiCopy.Size = New System.Drawing.Size(181, 26)
+        Me.tmiCopy.Text = "Copy"
+        '
+        'tmiPaste
+        '
+        Me.tmiPaste.Name = "tmiPaste"
+        Me.tmiPaste.Size = New System.Drawing.Size(181, 26)
+        Me.tmiPaste.Text = "Paste"
+        '
+        'tsmHelp
+        '
+        Me.tsmHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tmiAbout})
+        Me.tsmHelp.Name = "tsmHelp"
+        Me.tsmHelp.Size = New System.Drawing.Size(53, 24)
+        Me.tsmHelp.Text = "Help"
+        '
+        'tmiAbout
+        '
+        Me.tmiAbout.Name = "tmiAbout"
+        Me.tmiAbout.Size = New System.Drawing.Size(181, 26)
+        Me.tmiAbout.Text = "About"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'txtContent
+        '
+        Me.txtContent.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtContent.Location = New System.Drawing.Point(0, 31)
+        Me.txtContent.Multiline = True
+        Me.txtContent.Name = "txtContent"
+        Me.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtContent.Size = New System.Drawing.Size(582, 322)
+        Me.txtContent.TabIndex = 3
+        Me.tltToolTip.SetToolTip(Me.txtContent, "Enter text for a file here")
+        '
+        'frmTextEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 353)
+        Me.Controls.Add(Me.txtContent)
+        Me.Controls.Add(Me.mnsMenu)
+        Me.MainMenuStrip = Me.mnsMenu
+        Me.Name = "frmTextEditor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Text Editor"
+        Me.mnsMenu.ResumeLayout(False)
+        Me.mnsMenu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents mnsMenu As MenuStrip
+    Friend WithEvents tsmFile As ToolStripMenuItem
+    Friend WithEvents tmiOpen As ToolStripMenuItem
+    Friend WithEvents tsmEdit As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents txtContent As TextBox
+    Friend WithEvents tmiSaveAs As ToolStripMenuItem
+    Friend WithEvents tsmHelp As ToolStripMenuItem
+    Friend WithEvents tmiAbout As ToolStripMenuItem
+    Friend WithEvents tmiNew As ToolStripMenuItem
+    Friend WithEvents tmiSave As ToolStripMenuItem
+    Friend WithEvents tmiClose As ToolStripMenuItem
+    Friend WithEvents tmiExit As ToolStripMenuItem
+    Friend WithEvents tmiCut As ToolStripMenuItem
+    Friend WithEvents tmiCopy As ToolStripMenuItem
+    Friend WithEvents tmiPaste As ToolStripMenuItem
+    Friend WithEvents tltToolTip As ToolTip
+End Class
---
+++ NETD-Lab5-EH-Final/NETD-Lab5-EH/Form1.vb
@@ -0,0 +1,162 @@
+﻿'****************************************************************
+'* Name: Evan Heath
+'* Student ID: 100710750
+'* Date: Mar 29th, 2019
+'* Program Name: NETD Lab 5 - Text Editor
+'* Purpose: A generic text editor which can create, load, and save files. It can also cut, copy, and paste text for you
+'****************************************************************
+
+Option Strict On
+
+Imports System.IO
+
+Public Class frmTextEditor
+
+    Dim saved As Boolean = True
+    Dim openedFile As String = Nothing
+
+    Private Sub tmiNew_Click(sender As Object, e As EventArgs) Handles tmiNew.Click
+        If SaveProcedure() Then
+            txtContent.Text = ""
+            saved = True
+            openedFile = Nothing
+        End If
+    End Sub
+
+    Private Sub tmiOpen_Click(sender As Object, e As EventArgs) Handles tmiOpen.Click
+        If SaveProcedure() Then
+            SelectAndDisplayFile()
+            saved = True
+        End If
+    End Sub
+
+    Private Sub tmiSave_Click(sender As Object, e As EventArgs) Handles tmiSave.Click
+        If openedFile = Nothing Then
+            SelectAndSaveFile()
+        Else
+            SaveFile(openedFile)
+        End If
+    End Sub
+
+    Private Sub tmiSaveAs_Click(sender As Object, e As EventArgs) Handles tmiSaveAs.Click
+        SelectAndSaveFile()
+    End Sub
+
+    Private Sub tmiClose_Click(sender As Object, e As EventArgs) Handles tmiClose.Click
+        If SaveProcedure() Then
+            txtContent.Text = ""
+            saved = True
+            openedFile = Nothing
+        End If
+    End Sub
+
+    Private Sub tmiExit_Click(sender As Object, e As EventArgs) Handles tmiExit.Click
+        If SaveProcedure() Then
+            Close()
+        End If
+    End Sub
+
+    Private Sub tmiCut_Click(sender As Object, e As EventArgs) Handles tmiCut.Click
+        ' Save selected text to clipboard
+        My.Computer.Clipboard.SetText(txtContent.SelectedText)
+
+        ' Erase selected text
+        txtContent.SelectedText = String.Empty
+    End Sub
+
+    Private Sub tmiCopy_Click(sender As Object, e As EventArgs) Handles tmiCopy.Click
+        ' Save selected text to clipboard
+        My.Computer.Clipboard.SetText(txtContent.SelectedText)
+    End Sub
+
+    Private Sub tmiPaste_Click(sender As Object, e As EventArgs) Handles tmiPaste.Click
+        ' Paste clipboard text
+        txtContent.SelectedText = My.Computer.Clipboard.GetText()
+    End Sub
+
+    Private Sub tmiAbout_Click(sender As Object, e As EventArgs) Handles tmiAbout.Click
+        Dim popup As MsgBoxResult = MsgBox("NETD-2202 Lab 5 - Text Editor By: E. Heath", MsgBoxStyle.OkOnly, "About")
+    End Sub
+
+    Private Sub txtContent_TextChanged(sender As Object, e As EventArgs) Handles txtContent.TextChanged
+        saved = False
+    End Sub
+
+    Public Sub SelectAndDisplayFile()
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Try
+                ' Open reader
+                Dim reader As New StreamReader(OpenFileDialog1.FileName)
+                ' Read all data / text
+                txtContent.Text = reader.ReadToEnd()
+                ' Close the file
+                reader.Close()
+                ' Set the opened file
+                openedFile = OpenFileDialog1.FileName
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString())
+            End Try
+        End If
+    End Sub
+
+    Public Function PromptSave() As Integer
+        If Not saved Then
+            Dim msg As MsgBoxResult = MsgBox("Would you like to save this file?", MsgBoxStyle.YesNoCancel, "Save File?")
+
+            If msg = MsgBoxResult.Yes Then
+                Return 2
+            ElseIf msg = MsgBoxResult.No Then
+                Return 1
+            Else
+                Return 0
+            End If
+        Else
+            Return 1
+        End If
+    End Function
+
+    Public Sub SelectAndSaveFile()
+        ' Set a filter to find a file that is a txt file
+        SaveFileDialog1.Filter = "TXT Files (*.txt*)|*.txt"
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            SaveFile(SaveFileDialog1.FileName)
+            openedFile = SaveFileDialog1.FileName
+        End If
+    End Sub
+
+    Public Sub SaveFile(ByVal file As String)
+        Try
+            ' Write the text to the file
+            My.Computer.FileSystem.WriteAllText(file, txtContent.Text(), False)
+            saved = True
+        Catch exc As Exception
+            Console.WriteLine(exc.ToString())
+        End Try
+    End Sub
+
+    Public Function SaveProcedure() As Boolean
+        Dim save As Integer = PromptSave()
+        If Not save = 0 Then
+            If save = 2 Then
+                If openedFile = Nothing Then
+                    SelectAndSaveFile()
+                Else
+                    SaveFile(openedFile)
+                End If
+            End If
+            ' Return True because the user didn't select Cancel
+            Return True
+        End If
+        ' Return False because the user cancelled the action
+        Return False
+    End Function
+
+    Private Sub frmTextEditor_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+        txtContent.Size = New Size(Me.Size.Width - 18, Me.Size.Width - 78)
+    End Sub
+
+    Private Sub frmTextEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtContent.Size = New Size(582, 322)
+    End Sub
+
+End Class
